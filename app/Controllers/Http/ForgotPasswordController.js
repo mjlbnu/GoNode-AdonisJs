83c749ff0aa412ca991e91d23f5c8d85c5fa82c4
mjlbnu <mjlbnu@gmail.com>
2019-09-24 22:32:48 -0300

    Envio de e-mail para reset de senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,6 +14,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('mjlbnu@gmail.com', 'Marcio | GoNode')
+            .subject('recuperação de senha')
+        }
+      )
     } catch (error) {
       return response
         .status(error.status)
